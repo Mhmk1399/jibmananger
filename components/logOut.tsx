51ef0f376394b8600f39c482b888e82b51280930
Hossein <141348426+SHEKAF1234@users.noreply.github.com>
2025-01-07 11:58:59 +0330

    Update logOut.tsx

--- components/logOut.tsx
+++ components/logOut.tsx
@@ -64,7 +64,7 @@ const LogoutModal: React.FC<LogoutModalProps> = ({ isOpen, onClose }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="fixed inset-0 bg-black/10 backdrop-blur-sm flex items-center justify-center z-50"
+      className="fixed inset-0 backdrop-blur-sm flex items-center justify-center z-50"
       >
       <motion.div
          initial={{ opacity: 0, scale: 0.95 }}
@@ -74,13 +74,13 @@ const LogoutModal: React.FC<LogoutModalProps> = ({ isOpen, onClose }) => {
            stiffness: 50,
            duration: 0.5,
          }}
-        className="bg-white bg-opacity-30 border border-white/50 backdrop-blur-xl rounded-xl p-8 w-96 mx-4"
-        dir="rtl"
+         className="bg-white/85 border border-white/20 backdrop-blur-sm rounded-xl p-8 w-96 mx-4"
+         dir="rtl"
       >
-        <h2 className="text-xl font-bold mb-4 text-gray-100 border-b border-white pb-2">
+        <h2 className="text-xl font-bold mb-4 text-purple-400 border-b border-black/20 pb-2">
           خروج از حساب کاربری
         </h2>
-        <p className="text-gray-200 mb-6">
+        <p className="text-gray-400 mb-6">
           آیا مطمئن هستید که می‌خواهید خارج شوید؟
         </p>
         <div className="flex justify-start gap-2">
